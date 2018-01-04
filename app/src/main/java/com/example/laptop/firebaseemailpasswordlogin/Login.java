b029c518a8185a1c14a17af422eaa41f3699604e
Prince-Arora <19princearora95@gmail.com>
2018-01-04 19:55:19 +0530

    login done

--- app/src/main/java/com/example/laptop/firebaseemailpasswordlogin/Login.java
+++ app/src/main/java/com/example/laptop/firebaseemailpasswordlogin/Login.java
@@ -1,13 +1,48 @@
 package com.example.laptop.firebaseemailpasswordlogin;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-
+    private EditText email;
+    private EditText pass;
+    private Button btn;
+    FirebaseAuth Auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Auth=FirebaseAuth.getInstance();
+        email=(EditText)findViewById(R.id.Email_edit_text);
+        pass=(EditText)findViewById(R.id.password_edittext);
+    }
+    public void Login1(View v)
+    {
+        final ProgressDialog progressDialog=ProgressDialog.show(Login.this,"Please wait....","Processing",true);
+        Auth.signInWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+        progressDialog.dismiss();
+        if(task.isSuccessful())
+        {
+            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+            Intent i=new Intent(Login.this,viewprofile.class);
+            i.putExtra("Email",Auth.getCurrentUser().getEmail());
+            startActivity(i);
+        }
+    }
+});
     }
 }
--- app/src/main/java/com/example/laptop/firebaseemailpasswordlogin/viewprofile.java
+++ app/src/main/java/com/example/laptop/firebaseemailpasswordlogin/viewprofile.java
@@ -2,12 +2,15 @@ package com.example.laptop.firebaseemailpasswordlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class viewprofile extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewprofile);
+        tv=(TextView)findViewById(R.id.viewprofile);
+        tv.setText(getIntent().getExtras().getString("Email"));
     }
 }
